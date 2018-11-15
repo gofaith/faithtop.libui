91665277f2be8fd5c6545e7945fb46e2d96a2fcf
StevenZack <stevenzack@qq.com>
2018-11-15 16:38:13 +0800

    Just a Backup

--- box.go
+++ box.go
@@ -15,6 +15,13 @@ func VBox() *FBox {
 	v.view = v.v
 	return v
 }
+func HBox() *FBox {
+	v := &FBox{}
+	v.v = ui.NewHorizontalBox()
+	v.view = v.v
+	return v
+}
+
 func (v *FBox) Append(is ...IView) *FBox {
 	for _, i := range is {
 		v.v.Append(i.getBaseView().view, i.getBaseView().stretch)
--- win.go
+++ win.go
@@ -7,11 +7,22 @@ import (
 type FWin struct {
 	v         *ui.Window
 	showAfter bool
+	onClosing func() bool
 }
 
-func Win(title string, w, h int, hasMenubar bool) *FWin {
+func Win(w, h int) *FWin {
 	v := &FWin{}
-	v.v = ui.NewWindow(title, w, h, hasMenubar)
+	v.v = ui.NewWindow("", w, h, true)
+	v.v.OnClosing(func(*ui.Window) bool {
+		if v.onClosing != nil {
+			return v.onClosing()
+		}
+		return true
+	})
+	return v
+}
+func (v *FWin) Title(t string) *FWin {
+	v.v.SetTitle(t)
 	return v
 }
 func (w *FWin) DeferShow() *FWin {
@@ -28,4 +39,24 @@ func (w *FWin) Add(i IView) *FWin {
 func (w *FWin) Show() *FWin {
 	w.v.Show()
 	return w
+}
+func (v *FWin) OnClose(f func() bool) *FWin {
+	v.onClosing = f
+	return v
+}
+func (v *FWin) MainQuitOnClose() *FWin {
+	v.onClosing = func() bool {
+		MainQuit()
+		return true
+	}
+	return v
+}
+func (v *FWin) VBox(is ...IView) *FWin {
+	v.Add(VBox().Append(is...))
+	return v
+}
+
+func (v *FWin) HBox(is ...IView) *FWin {
+	v.Add(HBox().Append(is...))
+	return v
 }
